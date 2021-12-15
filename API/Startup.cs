8922cca664d20b14ad8f48a6f687bcb70951942d
Danang Nurdiansyah <nattesc82@gmail.com>
2021-12-15 16:48:27 +0800

    master

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
@@ -28,65 +29,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITgandhisquotesRepository, TGandhisquotesRepository>();
-            services.AddScoped<ITsggschapterpagesRepository, TsggschapterpagesRepository>();
-            services.AddScoped<IZtbiblechapternamesRepository, zTbiblechapternamesRepository>();
-            services.AddScoped<IZtgitafullRepository, ZtgitafullRepository>();
-            services.AddScoped<ITswamigitascsvRepository, tswamigitascsvRepository>();
-            services.AddScoped<IYtquranRepository, ytquranRepository>();
-            services.AddScoped<ItsggsFinalRepository, tSGGSFinalRepository>();
-            services.AddScoped<IHadithsRepository, HadithsRepository>();
-            services.AddScoped<ITBibleRepository, TBibleRepository>();
-            services.AddScoped<ISggsRepository, SggsRepository>();
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddDbContext<DataContext>(options =>
-            {
-                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-                string connStr;
-
-                // Depending on if in development or production, use either Heroku-provided
-                // connection string, or development connection string from env var.
-                if (env == "Development")
-                {
-                    // Use connection string from file.
-                    connStr = _config.GetConnectionString("DefaultConnection");
-                }
-                else
-                {
-                    // Use connection string provided at runtime by Heroku.
-                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-                    // Parse connection URL to connection string for Npgsql
-                    connUrl = connUrl.Replace("postgres://", string.Empty);
-                    var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@")[1];
-                    var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("/")[1];
-                    var pgUser = pgUserPass.Split(":")[0];
-                    var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split(":")[0];
-                    var pgPort = pgHostPort.Split(":")[1];
-
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
-                }
-
-                // Whether the connection string came from the local development configuration file
-                // or from the environment variable from Heroku, use it to set up your DbContext.
-                options.UseNpgsql(connStr);
-            });
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options => {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                    };
-                });
+            services.AddIdentityServices(_config);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
